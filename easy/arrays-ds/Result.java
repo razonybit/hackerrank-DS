f31ff29e677b9ef1dc01d502bcfd79e93949fae3
Sergio Robles <razonybit@gmail.com>
2022-07-04 22:57:23 -0500

    Solution added

---
+++ easy/arrays-ds/Result.java
@@ -0,0 +1,11 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Result {
+
+	public static List<Integer> reverseArray(List<Integer> arr) {
+		Collections.reverse(arr);
+		return arr;
+	}
+
+}
